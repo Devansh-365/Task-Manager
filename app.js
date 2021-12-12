6dd5f6e23f89f790037285f0f9d5d62ca3ae30ee
Devansh Tiwari <devanshtiwari365@gmail.com>
2021-12-12 12:00:03 +0530

    added README.md

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('hello, world')
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+})
